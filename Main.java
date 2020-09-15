9bad8886bea01afd3c70333f2f582b6759666af8
tomad6909 <tomad6909@wrdsb.ca>
2020-09-15 21:16:02 +0000

    Finished A2Q5

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program tells the user if a number is divisible by either 5 or 3
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for the user
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter a number
+    System.out.println("Please enter a number to play FizzBuzz");
+
+    int number = input.nextInt();
+
+    // determine whether the number is divisible by 3 or 5
+    if (number % 3 == 0 && number % 5 == 0){
+      System.out.println("You should say FizzBuzz");
+    }else if (number % 5 == 0){
+      System.out.println("You should say Buzz");
+    }else if (number % 3 == 0){
+      System.out.println("You should say Fizz");
+    }
+    else {
+      System.out.println("You should say " + number);
+    }
     
   }
 }
